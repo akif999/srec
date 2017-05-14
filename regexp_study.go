1abcb9f5608748f652fb7bc1325d737c4dcbeed0
AKIF999 <aki01nf28@gmail.com>
2017-05-14 09:56:56 +0900

    各フィールドを型通りに個別に取得できるところまで実装

--- regexp_study.go
+++ regexp_study.go
@@ -7,13 +7,14 @@ import (
 	"log"
 	"os"
 	// "regexp"
+	"strconv"
 	"strings"
 )
 
 const ()
 
 type Srec struct {
-	srectype uint
+	srectype string
 	length   uint32
 	address  uint32
 	data     []byte
@@ -26,6 +27,7 @@ var (
 
 // TODO グループ化してマッチさせ、columnスプリットでフィールドを取り出す
 func main() {
+	srec := new(Srec)
 	kingpin.Parse()
 	// re := regexp.MustCompile("S1")
 
@@ -40,9 +42,36 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		ss := strings.Split(line, "")
-		if (ss[0] == "S") && (ss[1] == "1") {
-			fmt.Println(ss)
+
+		/* get srectype*/
+		srec.srectype = strings.Join(ss[:2], "")
+		if srec.srectype != "S1" {
+			continue
 		}
+		/* get length */
+		len, _ := strconv.ParseUint(strings.Join(ss[2:4], ""), 16, 32)
+		srec.length = uint32(len)
+		/* address */
+		addr, _ := strconv.ParseUint(strings.Join(ss[4:8], ""), 16, 32)
+		srec.address = uint32(addr)
+		/* get datafield*/
+		data := make([]byte, 0)
+		for i := 0; i < (4 + (int(len) * 2) - 2); i += 2 {
+			if i >= 8 {
+				b, _ := strconv.ParseUint(strings.Join(ss[i:i+2], ""), 16, 32)
+				data = append(data, byte(b))
+			}
+		}
+		/* get checksum*/
+		csum, _ := strconv.ParseUint(strings.Join(ss[4+(int(len)*2)-2:(4+(int(len)*2)-2)+2], ""), 16, 32)
+		srec.checksum = byte(csum)
+
+		fmt.Printf("%s %02X %04X ", srec.srectype, srec.length, srec.address)
+		for _, b := range data {
+			fmt.Printf("%02X", b)
+		}
+		fmt.Printf(" %02X", srec.checksum)
+		fmt.Println()
 	}
 }
 
