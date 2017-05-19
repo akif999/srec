36a18af8bbe627266f6698b112c9900c30f18257
AKIF999 <aki01nf28@gmail.com>
2017-05-19 22:13:33 +0900

    ParseFile()の中身を大まかにメソッド化

--- regexp_study.go
+++ regexp_study.go
@@ -12,11 +12,11 @@ import (
 
 const ()
 
-type Srecs struct {
-	records []Srec
+type Srec struct {
+	records []Record
 }
 
-type Srec struct {
+type Record struct {
 	srectype string
 	length   uint32
 	address  uint32
@@ -29,17 +29,17 @@ var (
 )
 
 func main() {
-	srecs := new(Srecs)
+	srec := new(Srec)
 
 	kingpin.Parse()
 
-	srecs.ParseFile(filename)
-	srecs.PrintOnlyData()
-	srecs.WriteBinaryToFile()
+	srec.ParseFile(filename)
+	srec.PrintOnlyData()
+	srec.WriteBinaryToFile()
 }
 
-func (sr *Srecs) ParseFile(file *string) {
-	srec := new(Srec)
+func (srs *Srec) ParseFile(file *string) {
+	rec := new(Record)
 
 	fp, err := os.Open(*file)
 	if err != nil {
@@ -51,39 +51,47 @@ func (sr *Srecs) ParseFile(file *string) {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		ss := strings.Split(line, "")
+		sl := strings.Split(line, "")
 
-		/* get srectype*/
-		srectype := strings.Join(ss[:2], "")
-		switch srectype {
-		case "S1":
-			/* get fields */
-			len, _ := strconv.ParseUint(strings.Join(ss[2:4], ""), 16, 32)
-			addr, _ := strconv.ParseUint(strings.Join(ss[4:8], ""), 16, 32)
-			data := make([]byte, 0)
-			for i := 0; i < (4 + (int(len) * 2) - 2); i += 2 {
-				if i >= 8 {
-					b, _ := strconv.ParseUint(strings.Join(ss[i:i+2], ""), 16, 32)
-					data = append(data, byte(b))
-				}
-			}
-			csum, _ := strconv.ParseUint(strings.Join(ss[4+(int(len)*2)-2:(4+(int(len)*2)-2)+2], ""), 16, 32)
-			/* put fields */
-			srec.srectype = srectype
-			srec.length = uint32(len)
-			srec.address = uint32(addr)
-			srec.data = data
-			srec.checksum = byte(csum)
-			sr.records = append(sr.records, *srec)
-		case "S2":
-		case "S3":
-		default:
+		srectype := strings.Join(sl[:2], "")
+		rec.getSrecFields(srectype, sl)
+		if srectype == "S1" {
+			srs.records = append(srs.records, *rec)
 		}
-
 	}
 }
 
-func (sr *Srecs) PrintOnlyData() {
+func (rec *Record) getSrecFields(srectype string, sl []string) {
+	var len uint64
+	var addr uint64
+	var data []byte
+	var csum uint64
+
+	switch srectype {
+	case "S1":
+		len, _ = strconv.ParseUint(strings.Join(sl[2:4], ""), 16, 32)
+		addr, _ = strconv.ParseUint(strings.Join(sl[4:8], ""), 16, 32)
+		data = make([]byte, 0)
+		for i := 0; i < (4 + (int(len) * 2) - 2); i += 2 {
+			if i >= 8 {
+				b, _ := strconv.ParseUint(strings.Join(sl[i:i+2], ""), 16, 32)
+				data = append(data, byte(b))
+			}
+		}
+		csum, _ = strconv.ParseUint(strings.Join(sl[4+(int(len)*2)-2:(4+(int(len)*2)-2)+2], ""), 16, 32)
+	case "S2":
+	case "S3":
+	default:
+		return
+	}
+	rec.srectype = srectype
+	rec.length = uint32(len)
+	rec.address = uint32(addr)
+	rec.data = data
+	rec.checksum = byte(csum)
+}
+
+func (sr *Srec) PrintOnlyData() {
 	for _, r := range sr.records {
 		for _, b := range r.data {
 			fmt.Printf("%02X", b)
@@ -92,7 +100,7 @@ func (sr *Srecs) PrintOnlyData() {
 	}
 }
 
-func (sr *Srecs) WriteBinaryToFile() {
+func (sr *Srec) WriteBinaryToFile() {
 	writeFile, _ := os.OpenFile(*filename+".bin", os.O_WRONLY|os.O_CREATE, 0600)
 	writer := bufio.NewWriter(writeFile)
 	for _, r := range sr.records {
