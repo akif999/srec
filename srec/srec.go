7cd7b550a54dded467c2019724f846dd8afa4613
AKIF999 <aki01nf28@gmail.com>
2017-06-11 06:11:43 +0900

    srec関連のオブジェクトとメソッドをpackageへ切り出し

---
+++ srec/srec.go
@@ -0,0 +1,97 @@
+package srec
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const ()
+
+type Srec struct {
+	records []Record
+}
+
+type Record struct {
+	srectype string
+	length   uint32
+	address  uint32
+	data     []byte
+	checksum byte
+}
+
+var ()
+
+func (srs *Srec) ParseFile(file *string) {
+	rec := new(Record)
+
+	fp, err := os.Open(*file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+
+	scanner := bufio.NewScanner(fp)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		sl := strings.Split(line, "")
+
+		srectype := strings.Join(sl[:2], "")
+		rec.getSrecFields(srectype, sl)
+		if srectype == "S1" {
+			srs.records = append(srs.records, *rec)
+		}
+	}
+}
+
+func (rec *Record) getSrecFields(srectype string, sl []string) {
+	var len uint64
+	var addr uint64
+	var data []byte
+	var csum uint64
+
+	switch srectype {
+	case "S1":
+		len, _ = strconv.ParseUint(strings.Join(sl[2:4], ""), 16, 32)
+		addr, _ = strconv.ParseUint(strings.Join(sl[4:8], ""), 16, 32)
+		data = make([]byte, 0)
+		for i := 0; i < (4 + (int(len) * 2) - 2); i += 2 {
+			if i >= 8 {
+				b, _ := strconv.ParseUint(strings.Join(sl[i:i+2], ""), 16, 32)
+				data = append(data, byte(b))
+			}
+		}
+		csum, _ = strconv.ParseUint(strings.Join(sl[4+(int(len)*2)-2:(4+(int(len)*2)-2)+2], ""), 16, 32)
+	case "S2":
+	case "S3":
+	default:
+		return
+	}
+	rec.srectype = srectype
+	rec.length = uint32(len)
+	rec.address = uint32(addr)
+	rec.data = data
+	rec.checksum = byte(csum)
+}
+
+func (sr *Srec) PrintOnlyData() {
+	for _, r := range sr.records {
+		for _, b := range r.data {
+			fmt.Printf("%02X", b)
+		}
+		fmt.Println()
+	}
+}
+
+func (sr *Srec) WriteBinaryToFile(filename *string) {
+	writeFile, _ := os.OpenFile(*filename+".bin", os.O_WRONLY|os.O_CREATE, 0600)
+	writer := bufio.NewWriter(writeFile)
+	for _, r := range sr.records {
+		writer.Write(r.data)
+		writer.Flush()
+	}
+}
