af46134bb9812baf7c4e94af3b0fd7f5ec9d79cd
AKIF999 <aki01nf28@gmail.com>
2017-08-18 10:46:38 +0800

    chanage variable name (writeAddress -> wAddr)

--- srec.go
+++ srec.go
@@ -256,14 +256,14 @@ func (sr *Srec) Bytes() []byte {
 	return sr.dataBytes
 }
 
-func (sr *Srec) SetBytes(writeAddress uint32, wBytes []byte) error {
+func (sr *Srec) SetBytes(wAddr uint32, wBytes []byte) error {
 	if len(sr.dataRecords) == 0 {
 		return fmt.Errorf("byte data is empty. call PaeseFile() or maybe srec file has no S1~3 records.")
 	}
-	if (writeAddress < sr.startAddress) || (writeAddress > sr.endAddress) {
-		return fmt.Errorf("data address 0x%08X is out of srec range.", writeAddress)
+	if (wAddr < sr.startAddress) || (wAddr > sr.endAddress) {
+		return fmt.Errorf("data address 0x%08X is out of srec range.", wAddr)
 	}
-	start := int(writeAddress) - int(sr.startAddress)
+	start := int(wAddr) - int(sr.startAddress)
 	for i := 0; i < len(wBytes); i++ {
 		sr.dataBytes[start+i] = wBytes[i]
 	}
