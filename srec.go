ffb3157841b65c4fdb86a6958968dcedbb79b847
AKIF999 <aki01nf28@gmail.com>
2017-08-21 09:28:51 +0900

    split file(create bytes.go)

--- srec.go
+++ srec.go
@@ -310,21 +310,3 @@ func (sr *Srec) makePaddedBytes(startAddr uint32, endAddr uint32, lastRecordData
 	}
 	return nil
 }
-
-func (sr *Srec) Bytes() []byte {
-	return sr.dataBytes
-}
-
-func (sr *Srec) SetBytes(wAddr uint32, wBytes []byte) error {
-	if len(sr.dataRecords) == 0 {
-		return fmt.Errorf("byte data is empty. call PaeseFile() or maybe srec file has no S1~3 records.")
-	}
-	if (wAddr < sr.startAddress) || (wAddr > sr.endAddress) {
-		return fmt.Errorf("data address 0x%08X is out of srec range.", wAddr)
-	}
-	start := int(wAddr) - int(sr.startAddress)
-	for i := 0; i < len(wBytes); i++ {
-		sr.dataBytes[start+i] = wBytes[i]
-	}
-	return nil
-}
