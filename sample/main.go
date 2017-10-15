2f42e40574593c2f757442b20938ef0772dfd955
akif999 <aki01nf28@gmail.com>
2017-10-15 18:13:58 +0900

    update sample

--- sample/main.go
+++ sample/main.go
@@ -12,7 +12,6 @@ import (
 var (
 	filename = kingpin.Arg("Filename", "Srec filename").ExistingFile()
 	setAddr  = kingpin.Arg("SetAddress", "Address of setting Bytes").Uint32()
-	getAddr  = kingpin.Arg("GetAddress", "Address of getting Bytes").Uint32()
 	getSize  = kingpin.Arg("GetSize", "Size of getting Bytes").Uint32()
 )
 
@@ -32,11 +31,57 @@ func main() {
 		log.Fatal(err)
 	}
 
-	nr, err := srec.MakeRec("S1", 0xFFFC, []byte{0x12, 0x34, 0x56, 0x78})
+	nr, err := srec.MakeRec("S1", 0x0170, []byte{0x12, 0x34, 0x56, 0x78})
 	if err != nil {
 		log.Fatal(err)
 	}
 	sr.Records = append(sr.Records, nr)
 
-	fmt.Print(sr.String())
+	fmt.Println(sr.String())
+	fmt.Print(bytesToString(getBytes(sr)))
+}
+
+func getBytes(s *srec.Srec) []byte {
+	bytes := []byte{}
+
+	size := int32(getEndAddr(s) - getStartAddr(s))
+	for size >= 0 {
+		bytes = append(bytes, 0xFF)
+		size--
+	}
+
+	offset := getStartAddr(s)
+	for _, r := range s.Records {
+		addr := r.Address - offset
+		for _, b := range r.Data {
+			bytes[addr] = b
+			addr++
+		}
+	}
+	return bytes
+}
+
+func getStartAddr(s *srec.Srec) uint32 {
+	for _, r := range s.Records {
+		if r.Srectype == "S1" || r.Srectype == "S2" || r.Srectype == "S3" {
+			return r.Address
+		}
+	}
+	return 0x00000000
+}
+
+func getEndAddr(s *srec.Srec) uint32 {
+	return s.EndAddr()
+}
+
+func bytesToString(bt []byte) string {
+	s := ""
+	for i, b := range bt {
+		if i != 0 && i%16 == 0 {
+			s += fmt.Sprint("\n")
+		}
+		s += fmt.Sprintf("%02X", b)
+	}
+	s += fmt.Sprint("\n")
+	return s
 }
